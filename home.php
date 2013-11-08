d3c5fa1e50a3494ba3fedf07659db3c2a9dba121
simccaff <simccaff@uvm.edu>
2013-11-08 12:45:42 -0500

    First version of homepage

---
+++ home.php
@@ -0,0 +1,22 @@
+<?php include ("top.php"); ?>		
+
+<body id="home">
+
+<?php include ("header.php"); ?>		
+
+<?php include ("nav.php"); ?>		
+<br>
+<article id="main">
+
+<h2>Welcome to the Maxine Davis Glass Art Website!</h2>
+<img src="sushiplate.jpg" alt="sushiplate" width="800" height="600">
+<br>
+<p>This website is dedicated to Maxine Davis' Glass Art. Maxine is an aspiring artist who started out working with glass just as a hobby, but is now trying to sell some of her beloved artwork. Do you want to own a piece of this one-of-a-kind artwork? Do you want it custom tailored to suit your exact needs? On the following page is a form which can be filled out that will allow you to register as an official customer as well as place orders for glass art. the order form allows you to chose what category of glass art you would like and also lets you decide which colors you want included. This will ensure that you can get the custom piece that you really want, and can be a part of the creative process! Order Now!</p>
+<br>
+
+
+</article>
+<?php include ("footer.php"); ?>		
+
+</body>
+</html>

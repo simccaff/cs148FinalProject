a10b8e57e0bf679e4c7b3254ccb9e7e035623a0e
simccaff <simccaff@uvm.edu>
2013-11-08 13:00:22 -0500

    third version of home.php

--- home.php
+++ home.php
@@ -12,6 +12,7 @@
 <img src="sushiplate.jpg" alt="sushiplate" width="800" height="600">
 <p> This is going to be the full version of the website! these changes are rough and are mostly for committing to github for assignment 5.</p>
 <br>
+<p> I'm still not sure if this is what I'm going to end up doing for my final project so I haven't made many substantial changes yet in case I decide to switch topics.</p>
 <p>This website is dedicated to Maxine Davis' Glass Art. Maxine is an aspiring artist who started out working with glass just as a hobby, but is now trying to sell some of her beloved artwork. Do you want to own a piece of this one-of-a-kind artwork? Do you want it custom tailored to suit your exact needs? On the following page is a form which can be filled out that will allow you to register as an official customer as well as place orders for glass art. the order form allows you to chose what category of glass art you would like and also lets you decide which colors you want included. This will ensure that you can get the custom piece that you really want, and can be a part of the creative process! Order Now!</p>
 <br>
 
